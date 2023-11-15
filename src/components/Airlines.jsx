99782c784d436b2048ca1e50127982e65ab01d96
eamole <eamole@hotmail.com>
2023-11-15 13:43:24 +0000

    Slightly improved UI look
    Signed-off-by: eamole <eamole@hotmail.com>

--- src/components/Airlines.jsx
+++ src/components/Airlines.jsx
@@ -4,33 +4,51 @@ import { FaTimes } from "react-icons/fa"
 const Airlines = ({ airlines, onAirlineSelected,    onAirlineDelete }) => {
 
   const onAirlineSelectClick = (e) => {
-    const iata = e.target.id
+    const iata = e.target.parentNode.id
+    console.log("airline select ", e.target.parentNode, iata)
     onAirlineSelected(iata)
   }
 
   const onAirlineDeleteClick = (e) => {
-    const iata = e.target.parentNode.id
+    const iata = e.target.parentNode.parentNode.id
+    console.log("airline delete ", 
+    e.target.parentNode.parentNode, iata)
     onAirlineDelete(iata)
+  }
+
+  const Airline = ({text, key, id}) => {
+    return (
+      <div className="airline clickable"
+        onClick={onAirlineSelectClick}
+        id={id}
+        key={key}
+      >
+        <h3>
+          {text}
+
+          <FaTimes 
+                className="delete-icon"
+                onClick={onAirlineDeleteClick}
+              />
+
+        </h3>
+      
+      </div>
+    )
   }
 
   return (
     <div className="airlines">
       <h3>Airlines</h3>
-      <ul>
+      
         {
-          airlines.map(airline => <li 
+          airlines.map(airline => <Airline 
               key={airline.iata} 
               id={airline.iata} 
-              className="clickable" 
-              onClick={onAirlineSelectClick}>{airline.name}
-              <FaTimes 
-                className="delete-icon"
-                onClick={onAirlineDeleteClick}
-              />
-            </li>
+              text={airline.name}
+            />
           )
         }
-      </ul>
 
     </div>
   )
